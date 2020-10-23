3b8287afb85415cc32451c9cd9336437d4ac1d2d
readysloth <readysloth@protonmail.com>
2020-10-23 17:01:28 +0300

    feat(processor.py): Added ADD, ADC, AND, brought out common parts

--- processor.py
+++ processor.py
@@ -3,8 +3,30 @@ Thanks to http://www.gabrielececchetti.it/Teaching/CalcolatoriElettronici/Docs/i
 """
 import typing as t
 
+from enum import Enum, auto
+
 import memory
 from registers import Registers
+
+class OpType(Enum):
+    REG = auto()
+    MEM = auto()
+    IMM = auto()
+
+class OpArgs:
+    def __init__(self,
+                 to: t.Union[int, str],
+                 arg: t.Union[int, str],
+                 to_type: OpType,
+                 arg_type: OpType):
+        self.to = to
+        self.arg = arg
+        self.to_type = to_type
+        self.arg_type = arg_type
+
+    def op_type(self):
+        return (self.to_type, self.arg_type)
+
 
 class CPU:
     def __init__(self,
@@ -12,6 +34,21 @@ class CPU:
                  registers: Registers):
         self.ram = ram
         self.registers = registers
+
+    def __str__(self):
+        return str(self.registers)
+
+    def _get_args(self, arguments: OpArgs):
+        first_arg = getattr(self.registers, arguments.to)
+
+        if arguments.arg_type == OpType.REG:
+            second_arg = getattr(self.registers, arguments.arg)
+        elif arguments.arg_type == OpType.MEM:
+            second_arg = self.ram[arguments.arg]
+        elif arguments.arg_type == OpType.IMM:
+            second_arg = arguments.arg
+
+        return first_arg, second_arg
 
 
     def AAA(self):
@@ -48,3 +85,43 @@ class CPU:
             self.registers.FLAGS.AF = 0
             self.registers.FLAGS.CF = 0
         return self
+
+    def ADC(self, arguments: OpArgs):
+        cpu = self.ADD(arguments)
+        if arguments.to_type == OpType.REG:
+            reg_val = getattr(self.registers, arguments.to)
+            setattr(cpu.registers,
+                    arguments.to,
+                    reg_val + cpu.registers.FLAGS.CF)
+            return cpu
+
+        cpu.ram[arguments.to] += cpu.registers.FLAGS.CF
+
+        return cpu
+
+
+    def ADD(self, arguments: OpArgs):
+        first_arg, second_arg = self._get_args(arguments)
+        if arguments.to_type == OpType.REG:
+            setattr(self.registers,
+                    arguments.to,
+                    first_arg + second_arg)
+            return self
+
+        self.ram[arguments.to] += second_arg
+
+        return self
+
+
+    def AND(self, arguments: OpArgs):
+        first_arg, second_arg = self._get_args(arguments)
+        if arguments.to_type == OpType.REG:
+            setattr(self.registers,
+                    arguments.to,
+                    first_arg & second_arg)
+            return self
+
+        self.ram[arguments.to] &= second_arg
+
+        return self
+
